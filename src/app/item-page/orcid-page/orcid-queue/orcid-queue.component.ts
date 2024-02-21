570eae7cef7806dd796aa5beca3b3c1a9eb2c9fa
Gantner, Florian Klaus <florian.gantner@uni-bamberg.de>
2024-02-21 12:40:18 +0100

    orcid sync settings for product entity

--- src/app/item-page/orcid-page/orcid-queue/orcid-queue.component.ts
+++ src/app/item-page/orcid-page/orcid-queue/orcid-queue.component.ts
@@ -118,6 +118,10 @@ export class OrcidQueueComponent implements OnInit, OnDestroy {
     switch (orcidQueue.recordType.toLowerCase()) {
       case 'publication':
         return 'fas fa-book';
+      case 'product':
+        return 'fas fa-database';
+      case 'funding':
+        return 'fa fa-wallet';
       case 'project':
         return 'fas fa-wallet';
       case 'education':

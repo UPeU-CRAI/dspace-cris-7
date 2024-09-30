e9750296e0f3f4a3ac3e293a3b71b14a0832706a
Alisa Ismailati <alisa.ismailati@4science.com>
2024-09-30 15:31:18 +0200

    [CST-16756] announce file uploading progress

--- src/app/shared/upload/uploader/uploader.component.ts
+++ src/app/shared/upload/uploader/uploader.component.ts
@@ -15,6 +15,7 @@ import {
 import { TranslateModule } from '@ngx-translate/core';
 import uniqueId from 'lodash/uniqueId';
 import {
+  FileItem,
   FileUploader,
   FileUploadModule,
 } from 'ng2-file-upload';
@@ -34,6 +35,7 @@ import {
 } from '../../empty.util';
 import { UploaderOptions } from './uploader-options.model';
 import { UploaderProperties } from './uploader-properties.model';
+import { LiveRegionService } from '../../live-region/live-region.service';
 
 @Component({
   selector: 'ds-uploader',
@@ -101,6 +103,11 @@ export class UploaderComponent implements OnInit, AfterViewInit {
   public isOverBaseDropZone = observableOf(false);
   public isOverDocumentDropZone = observableOf(false);
 
+  /**
+   * Set of progress values that have been announced to screen readers
+   */
+  private announcedProgress: Set<number> = new Set();
+
   @HostListener('window:dragover', ['$event'])
   onDragOver(event: any) {
 
@@ -118,6 +125,7 @@ export class UploaderComponent implements OnInit, AfterViewInit {
     private dragService: DragService,
     private tokenExtractor: HttpXsrfTokenExtractor,
     private cookieService: CookieService,
+    private liveRegionService: LiveRegionService
   ) {
   }
 
@@ -199,7 +207,25 @@ export class UploaderComponent implements OnInit, AfterViewInit {
       this.uploader.cancelAll();
     };
     this.uploader.onProgressAll = () => this.onProgress();
-    this.uploader.onProgressItem = () => this.onProgress();
+    // Live region service setup
+    this.liveRegionService.setMessageTimeOutMs(1500);
+    this.liveRegionService.clear();
+    this.uploader.onProgressItem = (fileItem: FileItem, progress: any) => {
+      this.announceProgress(progress);
+      this.onProgress()
+    };
+  }
+
+  /**
+   * Announce the progress of the upload to screen readers
+   * @param progress
+   */
+  private announceProgress(progress: any) {
+    if (!this.announcedProgress.has(progress)) {
+      this.announcedProgress.add(progress);
+      const message = progress + '%';
+      this.liveRegionService.addMessage(message);
+    }
   }
 
   /**
